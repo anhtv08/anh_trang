a47973e6661218331c30c0f7124c96d984532b7f
anhtv08 <anhtv010888@gmail.com>
2015-01-14 22:59:01 +0700

    Revert "test"
    This reverts commit 48a3bbbe6830b6a79afcbb936e0e126c6016a073.

--- angular_bootstrap_spring - Copy/src/main/java/au/com/example/persistence/dao/query/QueryString.java
+++
@@ -1,9 +0,0 @@
-package au.com.example.persistence.dao.query;
-
-import java.util.List;
-
-public interface QueryString {
-	public String getStatement();
-
-	public List<QueryParameter> getParameters();
-}
--- angular_bootstrap_spring - Copy/target/classes/import.sql
+++
@@ -1,4 +0,0 @@
-insert into Customer (first_name, last_name) values ('Foo', 'Bar');
-insert into Customer (first_name, last_name) values ('Jim', 'Sunny');
-insert into Customer (first_name, last_name) values ('Peter', 'Prone');
-insert into Customer (first_name, last_name) values ('Sam', 'Sully');
